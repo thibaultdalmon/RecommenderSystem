aa3fcfc457f0cb87059a2c93fa2e2ec76f8bc2ef
egoutierre <emmanuel.goutierre@mac.com>
2019-02-07 14:50:59 +0100

    clean docker compose

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,37 @@
-import argparse
+from flask import Flask, request, jsonify
+from OldEnv.Interface import Interface
+from OldEnv.Trainer import Trainer
 
-parser = argparse.ArgumentParser()
+app = Flask(__name__)
 
-parser.add_argument('--user_id', type=str, default='R3EIFXNYY6XMBXBR01BK')
-parser.add_argument('--ip_address_old_env', type=str, default='52.47.62.31')
-parser.add_argument('--ip_address_new_env', type=str, default='35.180.46.68')
 
-args = parser.parse_args()
+class Argument:
+    pass
+
+
+args = Argument
+args.user_id = 'R3EIFXNYY6XMBXBR01BK'
+args.ip_address_old_env = '52.47.62.31'
+args.ip_address_new_env = '35.180.46.68'
+
+interface = Interface(args)
+trainer = Trainer(args)
+
+
+@app.route("/reset")
+def reset():
+    interface.reset()
+    trainer.train()
+
+
+@app.route("/request", methods=['GET', 'POST'])
+def predict():
+    user_id = request.args.get('user_id')
+    item_id = request.args.get('item_id')
+    predicted_score = trainer.predict(user_id, item_id)
+    d = {'predicted_score': predicted_score}
+    return jsonify(d)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
